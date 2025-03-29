fdba67e963b29d5f73256f7de88118a5b06346a7
iamsarb001 <sarbbsandhu555@gmail.com>
2025-03-30 03:50:50 +0530

    card section - implemented

---
+++ src/components/CardSection.tsx
@@ -0,0 +1,37 @@
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "./ui/card"
+
+export const  CardSection = () => {
+
+    const QuizData = [
+        { id : 1 , title : " Choose your quiz type " , content : "Built for both students, lead capture and leisure, you can make your own trivia quiz, personality tests, polls, and surveys. Assign grades, certificates and more." },
+        { id : 2 , title : " Write your questions " , content : "  Add your question from more than 40 types, tick the correct answer and repeat. Hit the Themes tab to change the look and feel of your quiz." },
+        { id : 3 , title : " Share with your network" , content : " Send out your quiz via a direct link, embed on your website or share on social and start getting responses right away." },
+    ]
+
+    return (
+        <>
+         {QuizData?.map(i => {
+            return (
+                <div className="my-4">
+                    <Card className="w-[350px]">
+                    <CardHeader>
+                        <CardTitle className="font-bold text-2xl"> {i?.id}) {i?.title} </CardTitle>
+                    </CardHeader>
+                        <CardContent>
+                            <div>
+                                {i?.content}
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
+            )
+         })}
+        </>
+    )
+}
--- src/components/QuizSection.tsx
+++ src/components/QuizSection.tsx
@@ -1,13 +1,20 @@
+import { CardSection } from "./CardSection"
 import { Button } from "./ui/button"
 
 export const QuizSection = () => {
     return (
         <div className="px-12 py-2">
           <div className="grid justify-center items-center">
-             <div className="font-medium text-4xl"> Create an awesome quiz in minutes </div>
+             <div className="font-medium text-4xl flex items-center justify-center"> Create an awesome quiz in minutes </div>
              <p className="flex justify-center py-4"> Quiz Maker is the easiest way to make quizzes FREE </p>
              <div className="flex justify-center my-4">
-             <Button className="bg-orange-500 px-8 py-8 font-bold"> Generate Quiz Automatically (AI) </Button>
+                   <Button className="bg-orange-500 px-8 py-8 font-bold"> Generate Quiz Automatically (AI) </Button>
+             </div>
+             <div>
+                <div className="text-2xl font-medium flex justify-center py-4"> How to make a quiz </div>
+                <div className="grid md:grid-cols-2 items-center lg:grid-cols-3  gap-12 justify-center">
+                  <CardSection />
+                </div>
              </div>
           </div>
         </div>
